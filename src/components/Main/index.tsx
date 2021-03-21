f59edc4dd730bcaf189cdd14c32fbbd177e47866
gaiveknopf <avigaive@hotmail.com>
2021-03-20 21:25:05 -0300

    Last commit

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,17 @@
-const Main = () => (
-  <main>
-    <h1>Boilerplate</h1>
-  </main>
+import * as S from './styles';
+
+const Main = ({
+  title = 'Boilerplate',
+  description = 'Typescript, React, Graphql, And Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="imagem da logo da empresa que é paracido com uma caixa"
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+  </S.Wrapper>
 );
 
 export default Main;
